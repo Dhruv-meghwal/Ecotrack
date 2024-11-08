ff62741def4a2fd0a1acbc972b9d79b9b97d9977
Dhruv-meghwal <akumar253002@gmail.com>
2024-11-08 22:29:15 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,7 +11,6 @@ st.set_page_config(
     }
 )
 st.sidebar.title("Eco :green[Track]")
-st.sidebar.button("Calculator","/Calculator.py")
 st.title("Eco:green[Track]")
 st.text("The carbon footprint calculator")
 st.subheader("About Carbon Footprint")
